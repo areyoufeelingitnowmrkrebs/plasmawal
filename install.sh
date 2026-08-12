39fbe9a32b6d2ccc96f621a27472d83b41ce0001
Nathaniel Krebs <areyoufeelingitnowmrkrebs@gmail.com>
2026-08-12 13:12:08 -0700

    Update kwriteconfig6 command for KWin decoration

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ plasma-apply-colorscheme Pywal
 kvantummanager --set Pywal
 kwriteconfig6 --file kdeglobals --group KDE --key widgetStyle kvantum
 
-kwriteconfig6 --file kwinrc --group org.kde.kdecoration2 --key plugin org.kde.kwin.aurorae
+kwriteconfig6 --file kwinrc --group org.kde.kdecoration2 --key library org.kde.kwin.aurorae.v2
 kwriteconfig6 --file kwinrc --group org.kde.kdecoration2 --key theme __aurorae__svg__Utterly-Round-Dark
 qdbus6 org.kde.KWin /KWin reconfigure
 
